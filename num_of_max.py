cbc403879e4e9a936c9c501058010896135aff07
mbarcenas <misael.barcenas@yahoo.com>
2024-11-22 10:14:40 -0600

    finding the number of times a max occurs in an array

---
+++ num_of_max.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    candles.sort()
+    maxNum = candles[-1]
+    numOfMax = 0
+    for num in range(-1, -len(candles)-1, -1):
+        if candles[num] == maxNum:
+            numOfMax += 1
+    return numOfMax
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
