9dd18fcd852c2e4c3001a8202d4c7bb1eb5167f2
mbarcenas <misael.barcenas@yahoo.com>
2024-12-09 17:51:20 -0600

    passing objects in a sequential circular order

---
+++ allocating_objects.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'saveThePrisoner' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER m
+#  3. INTEGER s
+#
+
+def saveThePrisoner(n, m, s):
+    # Write your code here
+    counter = s
+    extra_turns = (m % n)- 1 +counter
+    if extra_turns == 0:
+        return n
+    if extra_turns > n:
+        return extra_turns - n
+          
+    return extra_turns
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        m = int(first_multiple_input[1])
+
+        s = int(first_multiple_input[2])
+
+        result = saveThePrisoner(n, m, s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
+
