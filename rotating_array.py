881cfa36f8a04fc2852bd0279008277150b4002d
mbarcenas <misael.barcenas@yahoo.com>
2024-12-12 17:03:18 -0600

    rotating an array k times and returning the new a[n]

---
+++ rotating_array.py
@@ -0,0 +1,60 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'circularArrayRotation' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER k
+#  3. INTEGER_ARRAY queries
+#
+
+def circularArrayRotation(a, k, queries):
+    # Write your code here
+    ans = []
+    if k == len(a):
+        for i in queries:
+            ans.append(a[i])
+    else:
+        index = k % len(a)
+        # if index > len(a):
+        #     index = index - len(a)
+        for i in queries:
+            i = i - index
+            ans.append(a[i])
+    return ans
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    q = int(first_multiple_input[2])
+
+    a = list(map(int, input().rstrip().split()))
+
+    queries = []
+
+    for _ in range(q):
+        queries_item = int(input().strip())
+        queries.append(queries_item)
+
+    result = circularArrayRotation(a, k, queries)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
